bff78ab1254e037da1bd25c036f67e8e9fd40ec0
IsayGordeev <gordeev.ip@phystech.edu>
2023-02-13 14:39:08 +0100

    added simple phase analys without checkout, only substring score function

--- manager.py
+++ manager.py
@@ -1,16 +1,16 @@
 from peak_classification import *
+from phase_classification import *
 
-a = Peaks(q, I, dI)
+peaks = Peaks(q, I, dI)
 
-a.background_reduction()
-a.filtering_negative()
-# a.peak_searching(height=0., prominence=prominence)
-# a.peak_verifying()
-# a.plot_diagramme()
+peaks.background_reduction()
+peaks.filtering_negative()
+peaks.phase_classification()
 
-a.phase_classification()
+# print('q', peaks.peaks_analysed_q)
+# print('I', peaks.peaks_analysed_I)
+# print(sorted(peaks.peaks_analysed_q) / min(peaks.peaks_analysed_q))
+# print('sigma', sorted(peaks.peaks_analysed_b))
 
-print('q', a.peaks_analysed_q)
-print('I', a.peaks_analysed_I)
-print(sorted(a.peaks_analysed_q) / min(a.peaks_analysed_q))
-print('sigma', sorted(a.peaks_analysed_b))
+phase = Phase(peaks.peaks_analysed_q, peaks.peaks_analysed_I, dI)
+phase.analyzing()
