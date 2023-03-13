3170e7dda35505a2d25af9c0873f9a2e98e6ef81
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-03-13 21:28:55 +0100

    arch

--- models_setup/model_build.py
+++ models_setup/model_build.py
@@ -39,55 +39,16 @@ class TinyVGG(nn.Module):
         x = self.conv_block_1(x)
         x = self.conv_block_2(x)
         return self.classifier(x)
-
-
-class TinyVGG(nn.Module):
-
-    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
-        super().__init__()
-        self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape,
-                      out_channels=hidden_units,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=hidden_units,
-                      out_channels=hidden_units,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,
-                         stride=2)
-        )
-        self.conv_block_2 = nn.Sequential(
-            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=hidden_units*16*16,
-                      out_features=output_shape)
-        )
-
-    def forward(self, x: torch.Tensor):
-        x = self.conv_block_1(x)
-        x = self.conv_block_2(x)
-        return self.classifier(x)
     
 
 
-
 class SASXpy(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
         self.block1 = nn.Sequential(
-            nn.Linear(),
-            nn.Conv2d(),
+            nn.Conv2d(in_channels=input_shape,
+                      output_channels=hidden_units,
+                      kernel_size=3, padding=1),
             nn.Linear(),
         )
 
