2ada818f347fdebda1daa8d11aa7d73eed1ec22a
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-06-16 01:25:49 +0200

    torchsummary?

--- saxs/check.py
+++ saxs/check.py
@@ -1,9 +1,17 @@
 from model import SASXTransformer
-from torchsummary import summary
+from torchinfo import summary
+
+from saxs.model import PatchEmbedding
 
 modelx = SASXTransformer()
 
+# print(modelx.parameters)
+# summary(model=modelx,
+#         input_size=(32, 1, 224,224),
+#         )
 
-summary(model=modelx,
-        input_size=(32,1, 224,224),
-        )
+summary(PatchEmbedding(), 
+        input_size=(32, 3, 224,224), 
+        col_names=["input_size", "output_size", "num_params", "trainable"],
+        col_width=20,
+        row_settings=["var_names"])
