c01ddb99b5345ac6b8cb55eeff9a1e173849263e
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-07-08 23:08:57 +0200

    pretrained state-of-art VIT

--- saxs/model_finetuning.py
+++ saxs/model_finetuning.py
@@ -1,1 +1,30 @@
-#load a model
+from torch.nn import Linear
+import torchvision
+from settings import DEVICE, PHASES_NUMBER,EMBEDDING_DIM
+
+from torchinfo import summary
+
+
+pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+
+pretrained_vit_B_16 = torchvision.models.vit_b_16(weights=pretrained_vit_weights).to(DEVICE)
+
+
+for layer in pretrained_vit_B_16.parameters():
+    layer.requires_grad = False
+
+pretrained_vit_B_16.heads = Linear(in_features=768, out_features=PHASES_NUMBER).to(DEVICE)  # FIRSTLY
+
+
+
+# summary(pretrained_vit_B_16,
+#         input_size=(1, 3, 224, 224),  # (batch_size, color_channels, height, width)
+#         col_names=["input_size", "output_size", "num_params", "trainable"],
+#         col_width=20,
+#         row_settings=["var_names"]
+#         )
+
+
+
+
+
--- saxs/settings.py
+++ saxs/settings.py
@@ -1,6 +1,12 @@
+import torch
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 COLOR_CHANNELS = 3
 IMAGE_DIM = 500 # len of q,I,dI
 EMBEDDING_DIM = 3*IMAGE_DIM
 HEADS = 12
 ATTENTION_BLOCKS = 3
 PATCH_SIZE = 16
+PHASES_NUMBER = 3
