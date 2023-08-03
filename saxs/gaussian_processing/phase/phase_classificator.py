967e51195f76bb01801d947167e08755e82bd99b
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-08-03 21:38:01 +0200

    default phase kernel

--- saxs/gaussian_processing/phase/phase_classificator.py
+++ saxs/gaussian_processing/phase/phase_classificator.py
@@ -3,9 +3,7 @@ import os
 
 import numpy as np
 
-from saxs.gaussian_processing.processing_outils import calculate_absolute_difference
 
-from saxs.gaussian_processing.phase.phase_application import PhaseApplication
 from saxs.gaussian_processing.settings_processing import ANALYSE_DIR_SESSIONS, ANALYSE_DIR_SESSIONS_RESULTS
 
 
@@ -17,12 +15,12 @@ class AbstractPhaseKernel:
         self.data = data
         self.sample = sample
         self.analyzed_q = np.array([])
+
         self.phases_number = len(self.phases_coefficients)
         self.distances = np.zeros(self.phases_number)
 
-
     def __call__(self, *args, **kwargs):
-        return self.point_classification()
+        return self.phase_classification()
 
     def set_directories(self, sample_name):
         self.filename_analyse_dir_phases = '../' + ANALYSE_DIR_SESSIONS + sample_name + '/phases'
@@ -34,26 +32,22 @@ class AbstractPhaseKernel:
         self.preprocessed_q = self.analyzed_q / self.analyzed_q[0]
         self.preprocessed_q = self.preprocessed_q[1:]
 
-    def absolute_sequence_comparison(self):
-        for i in range(self.phases_number):
-            self.distances[i] = calculate_absolute_difference(self.phases_coefficients[i], self.preprocessed_q)
 
-        # print(self.preprocessed_q)
-        # print(self.distances)
-
-    def point_classification(self):
+    def phase_classification(self):
         # self.set_directories(sample_name)
         self.read_sample_data()
 
         if len(self.analyzed_q) > 1:
-            self.preprocessing_q()
-            self.absolute_sequence_comparison()
+            self.phase_processing()
 
             return self.phases_dict[np.argmax(self.distances)]
         elif len(self.analyzed_q) == 1:
             return 'lamellar'
         else:
             return 'blanc'
+
+    def phase_processing(self):
+        pass
 
     def read_sample_data(self):
         self.analyzed_q = self.data[self.sample]['q']
