c77d14ee336df1c2f10960b0549781ff411c1e8b
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2024-07-11 15:22:04 +0200

    circular import changed

--- saxs/__init__.py
+++ saxs/__init__.py
@@ -1,7 +1,5 @@
 import os
 
-from . import data_processing, gaussian_processing, saxs_model
-
 PACKAGE_PATH = os.path.dirname(__file__)
 print(PACKAGE_PATH)
 DEFAULT_PHASES_PATH = os.path.join(PACKAGE_PATH, 'phases.json')
