d290a3c1cf3823c764e8b1461f1a34b4f142caf3
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-19 20:17:05 +0200

    delete torch dep

--- saxs/pipeline.py
+++ saxs/pipeline.py
@@ -4,7 +4,6 @@ import time
 from saxs.gaussian_processing.manager import Manager
 from saxs.gaussian_processing.phase.custom_phase_classification import *
 from saxs.saxs_model.phase_prediction import prediction_from_csv, prediction_from_npy
-import torch
 
 
 class PipelineAbstract:  # TODO TYPING CLASS
