5ba6fdc817c442bafbac6b3a250fb713957bbcbb
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-20 23:22:24 +0200

    small refactoring

--- saxs/saxs/processing/stage/peak/find_peak_stage.py
+++ saxs/saxs/processing/stage/peak/find_peak_stage.py
@@ -4,6 +4,7 @@
 
 from scipy.signal import find_peaks
 
+from saxs.logging.logger import logger
 from saxs.saxs.core.pipeline.condition.abstract_condition import SampleCondition
 from saxs.saxs.core.stage.abstract_cond_stage import (
     AbstractConditionalStage,
@@ -19,6 +20,8 @@ class FindAllPeaksStage(AbstractConditionalStage):
 
     def _process(self, stage_data):
         _params = {"x": stage_data.get_intensity_array()}
+
+        # indices of peaks
         _peaks, _props = find_peaks(**_params)
 
         processed_stage_data = stage_data.set_metadata_dict({"peaks": _peaks})
