88e80e1c50d4de8c7690386fd51fafaf27896e56
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-20 17:07:47 +0200

    abst stage condition

--- saxs/saxs/core/pipeline/condition/abstract_condition.py
+++ saxs/saxs/core/pipeline/condition/abstract_condition.py
@@ -5,6 +5,13 @@ from saxs.saxs.core.data.sample_objects import AbstractSampleMetadata
 
 class SampleCondition(ABC):
     @abstractmethod
-    def evaluate(self, sample: AbstractSampleMetadata) -> bool:
+    def evaluate(self, metadata: AbstractSampleMetadata) -> bool:
         """Return True if the condition passes for the given sample."""
+        pass
+
+
+class StageCondition(ABC):
+    @abstractmethod
+    def evaluate(self, metadata: AbstractSampleMetadata) -> bool:
+        """Return True if the condition passes for the given stage metadata."""
         pass
