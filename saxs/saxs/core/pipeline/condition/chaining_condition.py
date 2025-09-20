29a50b17cbb5bb6b5502408f240e8ff74cae5b52
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-20 14:53:39 +0200

    create chaining condition

---
+++ saxs/saxs/core/pipeline/condition/chaining_condition.py
@@ -0,0 +1,17 @@
+#
+# Created by Isai GORDEEV on 20/09/2025.
+#
+
+from saxs.saxs.core.data.sample_objects import AbstractSampleMetadata
+from saxs.saxs.core.pipeline.condition.abstract_condition import (
+    AbstractCondition,
+)
+
+
+class ChainingPeakCondition(AbstractCondition):
+    def __init__(self, key: str, expected_value):
+        self.key = key
+        self.expected_value = expected_value
+
+    def evaluate(self, sample: AbstractSampleMetadata) -> bool:
+        return sample.metadata.get(self.key) == self.expected_value
