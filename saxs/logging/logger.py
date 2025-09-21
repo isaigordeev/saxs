6be7062bf2997b3ae24b8485aacd8b6da69106d5
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-21 11:54:31 +0200

    logger format

--- saxs/logging/logger.py
+++ saxs/logging/logger.py
@@ -7,7 +7,8 @@ import sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s – %(module)s.%(funcName)s():%(lineno)d – %(message)s",
+    format="%(asctime)s [%(levelname)s] %(name)s – %(module)s.%(funcName)s():%"
+    "(lineno)d – %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
@@ -15,7 +16,9 @@ logging.basicConfig(
 
 def get_logger(name: str) -> logging.Logger:
     """Return a logger named after the module."""
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    logger.propagate = True
+    return logger
 
 
 logger = get_logger("application logger")
