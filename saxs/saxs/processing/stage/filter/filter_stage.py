7e4e8e00d6866a1505b704c4f17d1b7a908bbcfa
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-23 00:28:58 +0200

    moving average simple filtering

--- saxs/saxs/processing/stage/filter/filter_stage.py
+++ saxs/saxs/processing/stage/filter/filter_stage.py
@@ -1,6 +1,11 @@
 from saxs.saxs.core.stage.abstract_stage import AbstractStage
+from saxs.saxs.processing.functions import moving_average
 
 
 class FilterStage(AbstractStage):
-    def process(self, stage_data):
-        return super().process(stage_data)
+    def _process(self, stage_data):
+        intensity = stage_data.get_intensity_array()
+
+        filtered_intensity = moving_average(intensity, 10)
+
+        return stage_data.set_intensity(filtered_intensity), None
