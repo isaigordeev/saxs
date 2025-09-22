f4e155b6551c1fb4029352d1c383f4d3eae90d47
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 17:40:53 +0200

    create request fir find peak stage

--- saxs/saxs/processing/stage/peak/find_peak_stage.py
+++ saxs/saxs/processing/stage/peak/find_peak_stage.py
@@ -6,24 +6,28 @@ from typing import Type
 from scipy.signal import find_peaks
 
 from saxs.logging.logger import logger
+from saxs.saxs.core.data.scheduler_objects import AbstractSchedulerMetadata
 from saxs.saxs.core.data.stage_objects import AbstractStageMetadata
 from saxs.saxs.core.pipeline.condition.abstract_condition import SampleCondition
 from saxs.saxs.core.pipeline.condition.chaining_condition import (
     ChainingPeakCondition,
 )
-from saxs.saxs.core.stage.abstract_cond_stage import AbstractConditionalStage
+from saxs.saxs.core.stage.abstract_cond_stage import (
+    AbstractConditionalStage,
+    AbstractRequestingStage,
+)
+from saxs.saxs.core.stage.policy.single_stage_policy import (
+    SingleStageChainingPolicy,
+)
+from saxs.saxs.core.stage.request.abst_request import StageRequest
 from saxs.saxs.processing.stage.peak.process_peak_stage import (
     AProcessPeakStage,
 )
 
 
-class FindAllPeaksStage(AbstractConditionalStage):
-    def __init__(
-        self,
-        chaining_stage: Type[AProcessPeakStage] = AProcessPeakStage,
-        condition: SampleCondition = ChainingPeakCondition("dummy_key"),
-    ):
-        super().__init__(chaining_stage, condition)
+class FindAllPeaksStage(AbstractRequestingStage):
+    def __init__(self, policy: SingleStageChainingPolicy):
+        super().__init__(policy)
 
     def _process(self, stage_data):
         intensity = stage_data.get_intensity_array()
@@ -46,3 +50,11 @@ class FindAllPeaksStage(AbstractConditionalStage):
         )
 
         return processed_stage_data, {"peaks": peaks_indices}
+
+    def create_request(self) -> StageRequest:
+        eval_metadata = self.metadata
+        pass_metadata = AbstractStageMetadata(
+            {"current_peak_index": (self.metadata.unwrap().get("peaks"))}
+        )
+        scheduler_metadata = AbstractSchedulerMetadata(self.metadata.unwrap())
+        return StageRequest(eval_metadata, pass_metadata, scheduler_metadata)
