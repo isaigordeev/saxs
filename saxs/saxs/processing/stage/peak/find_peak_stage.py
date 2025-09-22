18bc4bcef8da40b0c58611b27ab08b83ec4ee2dc
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 18:44:40 +0200

    default policy for find all peaks

--- saxs/saxs/processing/stage/peak/find_peak_stage.py
+++ saxs/saxs/processing/stage/peak/find_peak_stage.py
@@ -17,18 +17,27 @@ from saxs.saxs.core.stage.abstract_cond_stage import (
     AbstractConditionalStage,
     AbstractRequestingStage,
 )
+from saxs.saxs.core.stage.policy.abstr_chaining_policy import ChainingPolicy
 from saxs.saxs.core.stage.policy.single_stage_policy import (
     SingleStageChainingPolicy,
 )
 from saxs.saxs.core.stage.request.abst_request import StageRequest
 from saxs.saxs.processing.stage.peak.process_peak_stage import (
-    AProcessPeakStage,
+    ProcessFitPeakStage,
 )
 
 
 class FindAllPeaksStage(AbstractRequestingStage):
     def __init__(self, policy: SingleStageChainingPolicy):
         super().__init__(policy)
+
+    @classmethod
+    def default_policy(cls) -> "ChainingPolicy":
+        # This default policy will automatically inject NextStage if Condition is true
+        return SingleStageChainingPolicy(
+            condition=ChainingPeakCondition("peaks"),
+            next_stage_cls=ProcessFitPeakStage,
+        )
 
     def _process(self, stage_data):
         intensity = stage_data.get_intensity_array()
