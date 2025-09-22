50dcf0b104cee727d871c5cf9259a81bf32b83d1
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 16:02:10 +0200

    scheduler metadata for sample metadata gathering

---
+++ saxs/saxs/core/data/scheduler_objects.py
@@ -0,0 +1,17 @@
+#
+# Created by Isai GORDEEV on 22/09/2025.
+#
+
+
+from dataclasses import dataclass, field
+from typing import Any, Dict
+
+from saxs.saxs.core.data.abstract_data import BaseDataType
+
+
+@dataclass(frozen=True)
+class AbstractSchedulerMetadata(BaseDataType):
+    values: Dict[str, Any] = field(default_factory=dict)
+
+    def describe(self) -> str:
+        return f"Stage scheduler metadata with keys: {list(self.values.keys())}"
