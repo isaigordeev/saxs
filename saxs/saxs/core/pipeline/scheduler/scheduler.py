b2999f57d637974b4232cd77ffa0b167388f102a
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 20:24:17 +0200

    logs for scheduler

--- saxs/saxs/core/pipeline/scheduler/scheduler.py
+++ saxs/saxs/core/pipeline/scheduler/scheduler.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from collections import deque
 from typing import List, Optional
 
+from saxs.logging.logger import logger
 from saxs.saxs.core.pipeline.scheduler.policy.insertion_policy import (
     AlwaysInsertPolicy,
     InsertionPolicy,
@@ -22,6 +23,9 @@ class AbstractScheduler(ABC):
         init_stages: Optional[List] = None,
         insertion_policy: Optional[InsertionPolicy] = None,
     ):
+        logger.info(
+            f"\n{'=' * 30}\n[Scheduler] Init_stages: {init_stages}'\n{'=' * 30}"
+        )
         self._queue = deque(init_stages or [])
         self._insertion_policy = insertion_policy or AlwaysInsertPolicy()
 
@@ -31,24 +35,58 @@ class AbstractScheduler(ABC):
 
 
 class BaseScheduler(AbstractScheduler):
-    """Executes stages sequentially and appends new requests to the end of the
-    queue."""
+    """Executes stages sequentially and appends new requests to the end of the queue."""
 
     def run(self, init_sample):
         queue = self._queue
         sample = init_sample
+        step = 1
+
+        logger.info(f"\n{'=' * 30}\n[Scheduler] Queue: {queue}'\n{'=' * 30}")
 
         while queue:
             stage = queue.popleft()
+            stage_name = stage.__class__.__name__
+
+            logger.info(
+                f"\n{'=' * 30}\n[Scheduler] Step {step}: Running stage {stage}'{stage_name}'"
+                f" on sample '{sample}'\n{'=' * 30}"
+            )
+
+            # Process stage
             sample = stage.process(sample)
 
-            # collect new stage requests
+            logger.info(
+                f"\n[Scheduler] Stage '{stage_name}' completed. Sample metadata: {sample.get_metadata_dict()}\n"
+            )
+
+            # Collect new stage requests
             requests: List[StageApprovalRequest] = stage.request_stage()
 
-            for req in requests:
-                if self._insertion_policy(req):  # <- policy decides
-                    queue.append(req.stage)
-                else:
-                    return sample
+            if requests:
+                logger.info(
+                    f"\n[Scheduler] Stage '{stage_name}' generated {len(requests)} request(s)."
+                )
+            else:
+                logger.info(
+                    f"\n[Scheduler] Stage '{stage_name}' generated no requests.\n"
+                )
 
+            for req in requests:
+                req_stage_name = req.stage.__class__.__name__
+                if self._insertion_policy(req):  # policy decides
+                    queue.append(req.stage)
+                    logger.info(
+                        f"\n[Scheduler] Request {req} approved → Stage '{req_stage_name}' appended to queue."
+                    )
+                else:
+                    logger.info(
+                        f"\n[Scheduler] Request {req} rejected → Stage '{req_stage_name}' not appended."
+                    )
+
+            step += 1
+
+        logger.info(
+            f"\n{'=' * 30}\n[Scheduler] Pipeline completed. Final sample metadata: {sample.get_metadata_dict()}\n{'=' * 30}"
+        )
         return sample
