664b41358b3efc16da3112f9377142640e292170
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 16:03:15 +0200

    partial process peak stage

--- saxs/saxs/processing/stage/peak/process_peak_stage.py
+++ saxs/saxs/processing/stage/peak/process_peak_stage.py
@@ -2,11 +2,13 @@
 # Created by Isai GORDEEV on 20/09/2025.
 #
 
+from saxs.saxs.core.data.sample import SAXSSample
 from saxs.saxs.core.pipeline.condition.abstract_condition import SampleCondition
 from saxs.saxs.core.stage.abstract_cond_stage import (
     AbstractConditionalStage,
     AbstractSelfRepeatingConditionalStage,
 )
+from saxs.saxs.processing.functions import parabole
 
 
 class AProcessPeakStage(AbstractConditionalStage):
@@ -18,8 +20,14 @@ class AProcessPeakStage(AbstractConditionalStage):
 
 
 class ProcessFitPeakStage(AProcessPeakStage):
-    def __init__(self, chaining_stage, condition: SampleCondition):
-        super().__init__(chaining_stage, condition)
+    def _process(self, stage_data: SAXSSample):
+        current_peak_index = stage_data.metadata.unwrap().get(
+            "current_peak_index"
+        )
 
-    def _process(self, stage_data):
+        def current_peak_parabole(x, sigma, ampl):
+            return parabole(
+                x, self.current_q_state[current_peak_index], sigma, ampl
+            )
+
         return stage_data, None
