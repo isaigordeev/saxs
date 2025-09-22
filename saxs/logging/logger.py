b93240fe680d45fabc07dc541f3a89ce4060cbbe
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-22 19:57:40 +0200

    pretty logger postponed

--- saxs/logging/logger.py
+++ saxs/logging/logger.py
@@ -1,24 +1,38 @@
-#
-# Created by Isai GORDEEV on 20/09/2025.
-#
-
 import logging
 import sys
 
+
+# Register PrettyLogger globally
+class PrettyLogger(logging.Logger):
+    def __init__(self, name, level=logging.NOTSET):
+        super().__init__(name, level)
+
+    def info(self, msg, *args, **kwargs):
+        # Automatically wrap messages with separators and newlines
+        if "sample" in kwargs:
+            sample = kwargs.pop("sample")
+            msg = f"\n{'=' * 30}\n[Scheduler] {msg} on sample '{sample}'\n{'=' * 30}"
+        super().info(msg, *args, **kwargs)
+
+
+logging.setLoggerClass(PrettyLogger)
+
+# Configure basic logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s – %(module)s.%(funcName)s():%"
-    "(lineno)d – %(message)s",
+    format="%(asctime)s [%(levelname)s] %(name)s – %(module)s.%(funcName)s():%(lineno)d – %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
 
 
 def get_logger(name: str) -> logging.Logger:
-    """Return a logger named after the module."""
+    """Return a PrettyLogger named after the module."""
     logger = logging.getLogger(name)
-    logger.propagate = True
     return logger
 
 
 logger = get_logger("application logger")
+
+# Example usage
+logger.info("Step 1: Running stage 'preprocessing'", sample="Sample_A")
