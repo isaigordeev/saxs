874c7716f1ca7c76de89c64447f257c2cef47357
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-23 22:54:35 +0200

    smooth hook

---
+++ saxs/saxs/processing/stage/filter/smooth_stage.py
@@ -0,0 +1,11 @@
+import numpy as np
+from saxs.saxs.core.stage.abstract_stage import AbstractStage
+
+
+class SmoothStage(AbstractStage):
+    def _process(self, stage_data):
+        intensity = stage_data.get_intensity_array()
+
+        new_intensity_state = np.maximum(intensity, 0)
+
+        return stage_data.set_intensity(new_intensity_state), None
