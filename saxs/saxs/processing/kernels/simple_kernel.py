576ac08d0ab82581ce2583fee00b47c202e6708b
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-27 22:24:13 +0200

    stem for base kernel

--- saxs/saxs/processing/kernels/simple_kernel.py
+++ saxs/saxs/processing/kernels/simple_kernel.py
@@ -3,6 +3,8 @@ from saxs.saxs.core.data.stage_objects import AbstractStageMetadata
 from saxs.saxs.core.pipeline.condition.chaining_condition import (
     ChainingPeakCondition,
 )
+from saxs.saxs.core.stage.abstract_stage import AbstractStage
+from saxs.saxs.core.stage.compose_stage import CompositeRequstingStage
 from saxs.saxs.core.stage.policy.single_stage_policy import (
     SingleStageChainingPolicy,
 )
@@ -38,3 +40,34 @@ class SimpleKernel(AbstractKernel):
                 ),
             ),
         ]
+
+
+class BaseKernel(AbstractKernel):
+    def create_sample(self, data: dict) -> "SAXSSample":
+        return SAXSSample(data=data)
+
+    def define_pipeline(self):
+        _composite_callback = self.define_composite_peak_stage()
+
+        return [
+            (CutStage, {"cut_point": 200}),
+            (FilterStage),
+            (BackgroundStage),
+            (
+                FindAllPeaksStage,
+                {},
+                SingleStageChainingPolicy(
+                    condition=ChainingPeakCondition("peaks"),
+                    next_stage_cls=_composite_callback,
+                ),
+            ),
+        ]
+
+    def define_composite_peak_stage(self) -> AbstractStage:
+        _params =      {   "main_stage_cls": ProcessFitPeakStage, 
+        "main_kwargs" :  {},
+        "before" : [] ,
+        "after" : [],
+        "policy": 
+        "metadata":}
+        return CompositeRequstingStage(**_params)
