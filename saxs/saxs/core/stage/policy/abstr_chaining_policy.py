c1e687bee88760f32b0e77f6c9b76bbe44c7cf2b
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-30 01:40:04 +0200

    add init to abst chaining policy

--- saxs/saxs/core/stage/policy/abstr_chaining_policy.py
+++ saxs/saxs/core/stage/policy/abstr_chaining_policy.py
@@ -1,13 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Type
 
+from saxs.saxs.core.pipeline.condition.abstract_condition import StageCondition
 from saxs.saxs.core.pipeline.scheduler.abstract_stage_request import (
     StageApprovalRequest,
 )
+from saxs.saxs.core.stage.abstract_stage import AbstractStage
 from saxs.saxs.core.stage.request.abst_request import StageRequest
 
 
 class ChainingPolicy(ABC):
+    def __init__(
+        self,
+        condition: "StageCondition",
+        next_stage_cls: Type[List[AbstractStage]],
+    ):
+        self.condition = condition
+        self.next_stage_cls = next_stage_cls
+
     @abstractmethod
     def request(
         self, request_metadata: StageRequest
