bc535b4acf065e1cde82f25b46d928806c74e0c6
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-10-02 01:40:26 +0200

    fix registry

--- saxs/saxs/core/kernel/spec/core/define_registry.py
+++ saxs/saxs/core/kernel/spec/core/define_registry.py
@@ -9,13 +9,15 @@ from saxs.saxs.processing.stage.peak.find_peak_stage import FindAllPeaksStage
 from saxs.saxs.processing.stage.peak.process_peak_stage import (
     ProcessFitPeakStage,
 )
-from saxs.saxs.stage.policy.single_stage_policy import SingleStageChainingPolicy
+from saxs.saxs.core.stage.policy.single_stage_policy import (
+    SingleStageChainingPolicy,
+)
 
 stage_registry = StageRegistry()
-stage_registry.register("cut", CutStage)
-stage_registry.register("filter", FilterStage)
-stage_registry.register("find_peaks", FindAllPeaksStage)
-stage_registry.register("process_fit_peak", ProcessFitPeakStage)
+stage_registry.register("CutStage", CutStage)
+stage_registry.register("FilterStage", FilterStage)
+stage_registry.register("FindAllPeaksStage", FindAllPeaksStage)
+stage_registry.register("ProcessFitPeakStage", ProcessFitPeakStage)
 
 policy_registry = PolicyRegistry()
-policy_registry.register("single_stage_policy", SingleStageChainingPolicy)
+policy_registry.register("SingleStageChainingPolicy", SingleStageChainingPolicy)
