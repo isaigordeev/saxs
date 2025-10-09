b2460320862ede546e3bb5466e08d85ee42415b4
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-10-09 03:06:24 +0200

    set compiler

---
+++ saxs/saxs/core/kernel/spec/back/kernel_compiler.py
@@ -0,0 +1,62 @@
+from typing import List
+from saxs.saxs.core.kernel.spec.back.buffer import Buffer
+from saxs.saxs.core.kernel.spec.back.pipeline_spec_compiler import (
+    PipelineSpecCompiler,
+)
+from saxs.saxs.core.kernel.spec.back.policy_builder import PolicyBuilder
+from saxs.saxs.core.kernel.spec.back.policy_linker import PolicyLinker
+from saxs.saxs.core.kernel.spec.back.runtime_spec import PolicySpec, StageSpec
+from saxs.saxs.core.kernel.spec.back.stage_builder import StageBuilder
+from saxs.saxs.core.kernel.spec.back.stage_linker import StageLinker
+from saxs.saxs.core.kernel.spec.front.declarative_specs import (
+    PolicyDeclSpec,
+    StageDeclSpec,
+)
+from saxs.saxs.core.kernel.spec.front.parser import DeclarativePipeline
+from saxs.saxs.core.stage.abstract_stage import AbstractStage
+from saxs.saxs.core.stage.policy.abstr_chaining_policy import ChainingPolicy
+
+
+class StageCompiler:
+    def __init__(self):
+        pass
+
+    def get_parser(self, path_to_yaml: str = "kernel.yaml"):
+        return DeclarativePipeline.from_yaml(path_to_yaml)
+
+    def compile(self):
+        parser: DeclarativePipeline = self.get_parser()
+
+        print(parser)
+
+        print(parser.policy_decl_specs)
+        print(parser.stage_decl_specs)
+
+        policy_decl_specs: Buffer[PolicyDeclSpec] = parser.policy_decl_specs
+        stage_decl_specs: List[StageDeclSpec] = parser.stage_decl_specs
+
+        compiler = PipelineSpecCompiler()
+
+        policy_specs: Buffer[PolicySpec] = compiler._build_policy_specs(
+            policy_decl_specs
+        )
+        stage_specs: Buffer[StageSpec] = compiler._build_stage_specs(
+            stage_decl_specs
+        )
+
+        stage_instance: Buffer[AbstractStage] = StageBuilder.build(stage_specs)
+        policy_instance: Buffer[ChainingPolicy] = PolicyBuilder.build(
+            policy_specs
+        )
+
+        print(stage_instance)
+        print(policy_instance)
+
+        linked_policy_instance: Buffer[ChainingPolicy] = PolicyLinker.link(
+            policy_specs, stage_instance, policy_instance
+        )
+        linked_stage_instance: Buffer[ChainingPolicy] = StageLinker.link(
+            stage_specs, stage_instance, policy_instance
+        )
+
+        return linked_stage_instance
