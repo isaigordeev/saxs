42ca56331851b2a998b61839631e8a972f9d3d69
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-10-19 18:08:02 +0200

    base kernel fix refactor

--- saxs/saxs/core/kernel/spec/core/base_kernel.py
+++ saxs/saxs/core/kernel/spec/core/base_kernel.py
@@ -1,20 +1,20 @@
+from typing import TYPE_CHECKING
+
 from saxs.logging.logger import logger
 from saxs.saxs.core.data.sample import SAXSSample
-from saxs.saxs.core.kernel.spec.back.buffer import Buffer
 from saxs.saxs.core.kernel.spec.back.kernel_compiler import BaseCompiler
 from saxs.saxs.core.kernel.spec.core.abstract_kernel import (
     AbstractKernel,
 )
 from saxs.saxs.core.pipeline.pipeline import Pipeline
-from saxs.saxs.core.pipeline.scheduler.policy.insertion_policy import (
-    InsertionPolicy,
-    SaturationInsertPolicy,
-)
-from saxs.saxs.core.pipeline.scheduler.scheduler import AbstractScheduler
-from saxs.saxs.core.stage.abstract_cond_stage import AbstractRequestingStage
 from saxs.saxs.core.stage.abstract_stage import AbstractStage
-from saxs.saxs.core.stage.policy.abstr_chaining_policy import ChainingPolicy
-from saxs.saxs.core.stage.policy.policy_registry import PolicyRegistry
+
+if TYPE_CHECKING:
+    from saxs.saxs.core.kernel.spec.back.buffer import Buffer
+    from saxs.saxs.core.pipeline.scheduler.scheduler import AbstractScheduler
+    from saxs.saxs.core.stage.policy.abstr_chaining_policy import (
+        ChainingPolicy,
+    )
 
 
 class BaseKernel(AbstractKernel):
@@ -30,10 +30,8 @@ class BaseKernel(AbstractKernel):
     def __init__(
         self,
         scheduler: "AbstractScheduler",
-        scheduler_policy: InsertionPolicy,
     ):
         self.scheduler = scheduler
-        self.scheduler_policy = scheduler_policy or SaturationInsertPolicy()
         self.execution_order: list[str] = []
 
     def build(self) -> None:
@@ -55,7 +53,6 @@ class BaseKernel(AbstractKernel):
         self.pipeline = Pipeline.with_stages(
             _initial_stages,
             scheduler=self.scheduler,
-            scheduler_policy=self.scheduler_policy,
         )
 
     def _get_initial_stage(
