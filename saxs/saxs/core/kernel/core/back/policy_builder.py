2eb097c70b005d36df92a9ccd6b9cd3c6f2a4d86
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-10-23 23:11:57 +0200

    fix cond init

--- saxs/saxs/core/kernel/core/back/policy_builder.py
+++ saxs/saxs/core/kernel/core/back/policy_builder.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from saxs.saxs.core.kernel.core.back.buffer import Buffer
 from saxs.saxs.core.kernel.core.back.runtime_spec import PolicySpec
+from saxs.saxs.core.pipeline.condition.abstract_condition import StageCondition
 from saxs.saxs.core.stage.policy.abstr_chaining_policy import ChainingPolicy
 
 
@@ -20,18 +21,25 @@ class PolicyBuilder:
         """
         policy_instances: Buffer[ChainingPolicy] = Buffer[ChainingPolicy]()
 
-        def build_policy_kwargs(_policy_spec: PolicySpec) -> dict[str, Any]:
-            _kwargs: dict[str, Any] = {}
-
-            _kwargs["condition"] = _policy_spec.condition
-            _kwargs["next_stage_cls"] = []
-
-            return _kwargs
-
         for policy_spec in policy_specs.values():
-            kwargs = build_policy_kwargs(policy_spec)
+            kwargs = PolicyBuilder.build_policy_kwargs(policy_spec)
             instance = policy_spec.policy_cls(**kwargs)
 
             policy_instances.register(policy_spec.id, instance)
 
         return policy_instances
+
+    @staticmethod
+    def build_policy_kwargs(_policy_spec: PolicySpec) -> dict[str, Any]:
+        _kwargs: dict[str, Any] = {}
+
+        _kwargs["condition"] = PolicyBuilder.build_condition(_policy_spec)
+        _kwargs["next_stage_cls"] = _policy_spec.next_stage_ids
+
+        return _kwargs
+
+    @staticmethod
+    def build_condition(_policy_spec: PolicySpec) -> StageCondition:
+        _kwargs: dict[str, Any] = {}
+        _kwargs = _policy_spec.condition_kwargs
+        return _policy_spec.condition(**_kwargs)
