7b4dc07f545c20433338c460cbe1961b5b5c97a5
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-11-02 21:10:28 +0100

    feat: introduce types for processing

--- saxs/saxs/processing/stage/peak/types.py
+++ saxs/saxs/processing/stage/peak/types.py
@@ -43,3 +43,43 @@ class PeakFindStageMetadata(TAbstractStageMetadata[PeakFindStageMetadataDict]):
             EBackMetadataKeys.PeakFind_COEF.value: 0.3,
         },
     )
+
+
+class EPeakProcessMetadataKeys(EMetadataSchemaKeys):
+    """Enum of keys used in PeakFindStageMetadataDict."""
+
+
+class PeakProcessStageMetadataDict(MetadataSchemaDict, total=False):
+    """
+    Schema for Cut stage metadata.
+
+    Attributes
+    ----------
+    cut_point : int
+        Index or position representing the cut point
+        in the SAXS data array.
+    """
+
+
+class PeakProcessStageMetadata(
+    TAbstractStageMetadata[PeakFindStageMetadataDict],
+):
+    """
+    Metadata object representing the Cut stage configuration.
+
+    Provides a builder-style, type-safe interface around
+    a `CutStageMetadataDict` dictionary, with convenient
+    accessors and default initialization.
+
+    Attributes
+    ----------
+    value : CutStageMetadataDict
+        Underlying metadata dictionary, defaulting to
+        `{"cut_point": 0}`.
+    """
+
+    value: PeakFindStageMetadataDict = field(
+        default_factory=lambda: {
+            EBackMetadataKeys.PeakFind_COEF.value: 0.3,
+        },
+    )
