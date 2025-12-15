77a9d561475e0c8f0d9bbe6fe674a32009cf9479
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-12-15 02:33:39 +0100

    feat: intro connection interface

--- saxs/producer/pkg/transport/stdio.go
+++ saxs/producer/pkg/transport/stdio.go
@@ -1,3 +1,35 @@
 package transport
 
+import (
+	"context"
+	"io"
+	"os"
+)
+
 type StdioTransport struct{}
+
+type Connection interface {
+	Read()
+	Write()
+	Close() error
+}
+
+type ioConn struct{}
+
+func newIOConn(rwc io.ReadWriteCloser) *ioConn
+
+func (c *ioConn) Close() error {
+	return nil
+}
+func (c *ioConn) Write() {
+	return
+}
+
+func (c *ioConn) Read() {
+	return
+}
+
+// Connect implements the [Transport] interface.
+func (*StdioTransport) Connect(context.Context) (Connection, error) {
+	return newIOConn(rwc{os.Stdin, nopCloserWriter{os.Stdout}}), nil
+}
